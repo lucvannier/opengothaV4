7a0d33d7ad6f9eb5cc3a8a41487e2bd1cdb0cc08
Luc <Luc@PC_Materiel_net>
2021-08-04 16:18:15 +0200

    V3.52.01

--- src/info/vannier/gotha/Gotha.java
+++ src/info/vannier/gotha/Gotha.java
@@ -39,7 +39,7 @@ public class Gotha {
     
     static Locale locale = Locale.getDefault();
     static final long GOTHA_VERSION = 352L;
-    static final long GOTHA_MINOR_VERSION = 0L;
+    static final long GOTHA_MINOR_VERSION = 1L;
     static final java.util.Date GOTHA_RELEASE_DATE = (new GregorianCalendar(2021, Calendar.AUGUST, 4)).getTime();
     
     // Should definitely stay below or equal to 32, due to internal limits in costValue() method
